2ba0586fc99a47b6b925f6058e07d760873fd476
Sean McGary <sean.mcgary@gmail.com>
2014-04-15 14:42:39 -0700

    creating api wrapper for delighted

--- examples/example.js
+++ examples/example.js
@@ -1,3 +1,20 @@
-var delighted = require('../');
+var delighted = new require('../')('DELIGHTED_API_KEY');
 
-console.log(delighted);
+
+// create a person
+delighted.person.create({
+
+})
+.then(function(response){
+	console.log(response);
+}, function(error){
+	console.log(error);
+});
+
+delighted.surveyResponse.list()
+.then(function(response){
+	console.log(response);
+}, function(error){
+	console.log(error);
+});
+
