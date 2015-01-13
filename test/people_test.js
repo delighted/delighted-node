e19b73c8e8012ca42455fe7b710829af89f53f4e
Parker Selbert <parker@sorentwo.com>
2015-01-12 21:43:25 -0600

    Implement initial Person prototype

---
+++ test/people_test.js
@@ -0,0 +1,30 @@
+var expect = require('chai').expect;
+var Person = require('../lib/Person');
+
+describe('Person', function() {
+  describe('#create', function() {
+    it('creates a new person and saves it to the server', function(done) {
+      var params = {
+        email: 'foo+testing@example.com',
+        name: 'Happy Person',
+        delay: 60,
+        properties: {
+          customer_id: 123,
+          country: 'USA',
+          local: 'en',
+          question_product_name: 'Classic Oxford'
+        }
+      }
+
+      var callback = function(person) {
+        expect(person.email).to.eq('foo+testing@example.com');
+        expect(person.delay).to.eq(60);
+        expect(person.properties).to.exist;
+
+        done();
+      }
+
+      Person.create(params, callback);
+    });
+  });
+});
