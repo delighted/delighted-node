14775084861465ad92f3960ad01d8912c9d81957
Parker Selbert <parker@sorentwo.com>
2015-01-14 17:23:13 -0600

    Configure and prepare resources on delighted

---
+++ test/delighted_test.js
@@ -0,0 +1,30 @@
+var expect    = require('chai').expect;
+var delighted = require('../lib/delighted');
+
+describe('delighted', function() {
+  var apiKey = 'abcd1234';
+
+  describe('resources', function() {
+    it('defines an instance of all resources', function() {
+      var instance = delighted(apiKey);
+
+      expect(instance).to.have.property('metrics');
+      expect(instance).to.have.property('person');
+      expect(instance).to.have.property('surveyRequest');
+      expect(instance).to.have.property('surveyResponse');
+      expect(instance).to.have.property('unsubscribe');
+    });
+  });
+
+  describe('apiKey', function() {
+    it('sets the encoded apiKey as the config.auth', function() {
+      var instance = delighted(apiKey);
+
+      // 'abcd1234:' => 'YWJjZDEyMzQ6'
+
+      expect(instance.config).to.exist;
+      expect(instance.config).to.have.property('auth');
+      expect(instance.config.auth).to.contain('YWJjZDEyMzQ6');
+    });
+  });
+});
