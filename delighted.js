938dd72a036f361b0cb46f6dbab375173970a696
Evan Tahler <evantahler@gmail.com>
2015-11-18 14:21:06 -0800

    Allow for options to be passed to client
    (as noted in the README for testing)

--- delighted.js
+++ delighted.js
@@ -1,5 +1,5 @@
 var Delighted = require('./lib/Delighted');
 
-module.exports = function(key) {
-  return new Delighted(key);
+module.exports = function(key, options) {
+  return new Delighted(key, options);
 };
