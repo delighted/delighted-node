e627372b32cf6dcbc0c64ea9778a66eb7cd63639
Mark Dodwell <markdodwell@gmail.com>
2016-12-15 17:35:15 -0800

    add some more coverage on errors

--- test/errors_test.js
+++ test/errors_test.js
@@ -4,9 +4,15 @@ var errors = require('../lib/Errors');
 describe('Errors', function() {
   it('maps status codes to custom errors', function() {
     var unauthorized = errors('401');
+    expect(unauthorized.name).to.eq('Error');
     expect(unauthorized.type).to.eq('AuthenticationError');
+    expect(unauthorized.message).to.eq(undefined);
+    expect(unauthorized.stack).to.be.a('string');
 
-    var unacceptable = errors('406');
+    var unacceptable = errors('406', 'foo');
+    expect(unauthorized.name).to.eq('Error');
     expect(unacceptable.type).to.eq('UnsupportedFormatRequestedError');
+    expect(unacceptable.message).to.eq('foo');
+    expect(unauthorized.stack).to.be.a('string');
   });
 });
